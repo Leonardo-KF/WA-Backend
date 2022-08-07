37f5715f2a498ee2558f0e13042fceaf63ca3a12
Leonardo Keiser Fleck <89053412+Leonardo-KF@users.noreply.github.com>
2022-08-07 15:14:23 -0300

    feat: refactoring findMovies useCase using factory

--- tests/usecases/find-movies.spec.ts
+++ tests/usecases/find-movies.spec.ts
@@ -7,7 +7,6 @@ class MoviesRepositorySpy implements IMoviesRepository {
 
   findMovies(skip: number, limit: number): Promise<Movie[]> {
     const findedMovies = this.movies.slice(skip, limit);
-    console.log(this.movies);
     return Promise.resolve(findedMovies);
   }
 
@@ -19,7 +18,7 @@ class MoviesRepositorySpy implements IMoviesRepository {
 class FindMoviesUseCase implements FindMovies {
   constructor(private readonly moviesRepository: IMoviesRepository) {}
   async execution(page: number, limit: number): Promise<Movie[]> {
-    const skipping = page * limit;
+    const skipping = (page - 1) * limit;
 
     const lastIndex = skipping + limit;
     const findedMovies = await this.moviesRepository.findMovies(
@@ -30,65 +29,91 @@ class FindMoviesUseCase implements FindMovies {
   }
 }
 
+const MakeSut = (): {
+  sut: FindMovies;
+  moviesRepositorySpy: MoviesRepositorySpy;
+} => {
+  const moviesRepositorySpy = new MoviesRepositorySpy();
+  moviesRepositorySpy.movies = [
+    {
+      banner: "test",
+      description: "test description",
+      director: "test director",
+      id: "1",
+      producer: "test producer",
+      title: "test title",
+    },
+    {
+      banner: "test",
+      description: "test description",
+      director: "test director",
+      id: "2",
+      producer: "test producer",
+      title: "test title",
+    },
+    {
+      banner: "test",
+      description: "test description",
+      director: "test director",
+      id: "3",
+      producer: "test producer",
+      title: "test title",
+    },
+    {
+      banner: "test",
+      description: "test description",
+      director: "test director",
+      id: "4",
+      producer: "test producer",
+      title: "test title",
+    },
+    {
+      banner: "test",
+      description: "test description",
+      director: "test director",
+      id: "5",
+      producer: "test producer",
+      title: "test title",
+    },
+    {
+      banner: "test",
+      description: "test description",
+      director: "test director",
+      id: "6",
+      producer: "test producer",
+      title: "test title",
+    },
+  ];
+  const sut = new FindMoviesUseCase(moviesRepositorySpy);
+  return { sut, moviesRepositorySpy };
+};
+
 describe("findMoviesUseCase", () => {
   it("should return the limit movies per execution", async () => {
-    const moviesRepository = new MoviesRepositorySpy();
-    moviesRepository.movies = [
-      {
-        banner: "test",
-        description: "test description",
-        director: "test director",
-        id: "1",
-        producer: "test producer",
-        title: "test title",
-      },
-      {
-        banner: "test",
-        description: "test description",
-        director: "test director",
-        id: "2",
-        producer: "test producer",
-        title: "test title",
-      },
-      {
-        banner: "test",
-        description: "test description",
-        director: "test director",
-        id: "3",
-        producer: "test producer",
-        title: "test title",
-      },
-      {
-        banner: "test",
-        description: "test description",
-        director: "test director",
-        id: "4",
-        producer: "test producer",
-        title: "test title",
-      },
-      {
-        banner: "test",
-        description: "test description",
-        director: "test director",
-        id: "5",
-        producer: "test producer",
-        title: "test title",
-      },
-      {
-        banner: "test",
-        description: "test description",
-        director: "test director",
-        id: "6",
-        producer: "test producer",
-        title: "test title",
-      },
-    ];
-    const sut = new FindMoviesUseCase(moviesRepository);
+    const { sut } = MakeSut();
 
     const limit = 2;
     const page = 1;
     const movieList = await sut.execution(page, limit);
 
     expect(movieList.length).toBe(limit);
+  });
+  it("should return the max number of movies if not have the limit in page", async () => {
+    const { sut } = MakeSut();
+
+    const limit = 4;
+    const page = 2;
+    const movieList = await sut.execution(page, limit);
+
+    expect(movieList.length).toBe(2);
+  });
+  it("should return the clean array if not have movies in page", async () => {
+    const { sut } = MakeSut();
+
+    const limit = 4;
+    const page = 3;
+    const movieList = await sut.execution(page, limit);
+
+    expect(movieList.length).toBe(0);
   });
 });
