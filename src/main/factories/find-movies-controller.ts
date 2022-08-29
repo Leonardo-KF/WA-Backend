1458e238d6641ddeab017c58b19a6212a38744f6
Leonardo Keiser Fleck <89053412+Leonardo-KF@users.noreply.github.com>
2022-08-29 08:23:29 -0300

    fix: fix path in factory to deploy

--- src/main/factories/find-movies-controller.ts
+++ src/main/factories/find-movies-controller.ts
@@ -1,7 +1,7 @@
 import { CountMoviesUseCase } from "@/data/usecases/count-movies";
 import { FindMoviesUseCase } from "@/data/usecases/find-movies";
 import { MoviesRepositoryMongo } from "@/infra/repositories/movies-repository-mongo";
-import { Controller } from "@/presentation/controllers/controller";
+import { Controller } from "../../presentation/controllers/controller";
 import { GetMoviesController } from "../presentation/controllers/get-movies-controller";
 
 export const makeFindMoviesController = (): Controller => {
--- src/main/factories/update-movies-controller.ts
+++ src/main/factories/update-movies-controller.ts
@@ -1,7 +1,7 @@
 import { SaveMoviesUseCase } from "@/data/usecases/save-movies";
 import { MoviesValidation } from "@/data/validation/movie-validation";
 import { MoviesRepositoryMongo } from "@/infra/repositories/movies-repository-mongo";
-import { Controller } from "@/presentation/controllers/Controller";
+import { Controller } from "../../presentation/controllers/controller";
 import { UpdateMoviesController } from "../presentation/controllers/update-movies-controller";
 import { GetRequest } from "../../infra/external/get-movies";
 
